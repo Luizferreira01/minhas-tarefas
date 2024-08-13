5efcc67da30ffae7ce41d1a53532d81be7cb0b6b
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-08-13 20:45:20 -0300

    Update index.html

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,6 +1,27 @@
+import Tarefa from '../components/Tarefa'
+import { Container } from './styles'
+
 const ListaDeTarefas = () => (
   <main>
-    <p>2 tarefas marcadas como: &quot;categoria&ldquo; e &ldquo;termo&quot;</p>
+    <Container></Container>
+    <p>2 tarefas marcadas como: &quot;categoria&quot; e &quot;termo&quot;</p>
+    <ul>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+      <li>
+        <Tarefa />
+      </li>
+    </ul>
   </main>
 )
 
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -6,6 +6,7 @@ const EstiloGlobal = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
     font-family: roboto, sans-serif;
+    list-style: none;
   }
 `
 
