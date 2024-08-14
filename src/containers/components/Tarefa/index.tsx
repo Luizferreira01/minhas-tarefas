e3e6b4604b9dc3d0626182e021852ea26f8a7af9
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-08-14 20:08:07 -0300

    Update index.tsx

--- src/containers/components/Tarefa/index.tsx
+++ src/containers/components/Tarefa/index.tsx
@@ -1,16 +1,30 @@
+import { useState } from 'react'
 import * as S from './styles'
 
-const Tarefa = () => (
-  <S.Card>
-    <S.Titulo>Nome Da Tarefa</S.Titulo>
-    <S.Tag>Importante</S.Tag>
-    <S.Tag>Pendente</S.Tag>
-    <S.Descricao />
-    <S.BarraAcoes>
-      <S.Botao>Editar</S.Botao>
-      <S.Botao>Remover</S.Botao>
-    </S.BarraAcoes>
-  </S.Card>
-)
+const Tarefa = () => {
+  const [estaEditando, setEstaEdiatando] = useState(false)
+
+  return (
+    <S.Card>
+      <S.Titulo>Nome Da Tarefa</S.Titulo>
+      <S.Tag>Importante</S.Tag>
+      <S.Tag>Pendente</S.Tag>
+      <S.Descricao />
+      <S.BarraAcoes>
+        {estaEditando ? (
+          <>
+            <S.Botao>Salvar</S.Botao>
+            <S.Botao onClick={() => setEstaEdiatando(false)}>Cancelar</S.Botao>
+          </>
+        ) : (
+          <>
+            <S.Botao onClick={() => setEstaEdiatando(true)}>Editar</S.Botao>
+            <S.Botao>Remover</S.Botao>
+          </>
+        )}
+      </S.BarraAcoes>
+    </S.Card>
+  )
+}
 
 export default Tarefa
