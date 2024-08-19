f4aef3f71e56662f90bac549aaed195c86cca79a
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-08-19 19:53:59 -0300

    Update index.tsx

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,28 +1,43 @@
 import Tarefa from '../components/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    titulo: 'estudar TypeScript',
+    descricao: 'ver a aula 3 da EBAC',
+    prioridade: 'importante',
+    status: 'pendente'
+  },
+  {
+    titulo: 'pagar a conta de internet',
+    descricao: 'baixar fatura no Gmail',
+    prioridade: 'urgente',
+    status: 'concluida'
+  },
+  {
+    titulo: 'Ir para a academia',
+    descricao: 'fazer treino B',
+    prioridade: 'importante',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => (
-  <main>
-    <Container></Container>
+  <Container>
     <p>2 tarefas marcadas como: &quot;categoria&quot; e &quot;termo&quot;</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
     </ul>
-  </main>
+  </Container>
 )
 
 export default ListaDeTarefas
--- src/containers/components/Tarefa/index.tsx
+++ src/containers/components/Tarefa/index.tsx
@@ -1,25 +1,34 @@
 import { useState } from 'react'
 import * as S from './styles'
 
-const Tarefa = () => {
+type Props = {
+  titulo: string
+  prioridade: string
+  status: string
+  descricao: string
+}
+
+const Tarefa = ({ descricao, prioridade, status, titulo }: Props) => {
   const [estaEditando, setEstaEdiatando] = useState(false)
 
   return (
     <S.Card>
-      <S.Titulo>Nome Da Tarefa</S.Titulo>
-      <S.Tag>Importante</S.Tag>
-      <S.Tag>Pendente</S.Tag>
+      <S.Titulo>{titulo}</S.Titulo>
+      <S.Tag prioridade={prioridade}>{prioridade}</S.Tag>
+      <S.Tag status={status}>{status}</S.Tag>
       <S.Descricao />
       <S.BarraAcoes>
         {estaEditando ? (
           <>
-            <S.Botao>Salvar</S.Botao>
-            <S.Botao onClick={() => setEstaEdiatando(false)}>Cancelar</S.Botao>
+            <S.BotaoSaalvar>Salvar</S.BotaoSaalvar>
+            <S.BotaoCancelarRemover onClick={() => setEstaEdiatando(false)}>
+              Cancelar
+            </S.BotaoCancelarRemover>
           </>
         ) : (
           <>
             <S.Botao onClick={() => setEstaEdiatando(true)}>Editar</S.Botao>
-            <S.Botao>Remover</S.Botao>
+            <S.BotaoCancelarRemover>Remover</S.BotaoCancelarRemover>
           </>
         )}
       </S.BarraAcoes>
