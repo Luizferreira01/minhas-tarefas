5266f26b7d2e56540eea6d0d7aec76cb1f8376c6
luiz felipe <luizfelipeeeeee13@gmail.com>
2024-09-16 18:48:43 -0300

    Update index.tsx

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,33 +1,30 @@
-import { useSelector } from 'react-redux';
-import Tarefa from '../components/Tarefa';
-import { Container } from './styles';
-import * as enums from '../../utils/enumns/tarefa';
+import { useSelector } from 'react-redux'
+import Tarefa from '../components/Tarefa'
+import { Container } from './styles'
+import * as enums from '../../utils/enumns/tarefa'
 
-import { RootReducer } from '../../store';
-
+import { RootReducer } from '../../store'
 
 const ListaDeTarefas = () => {
-  const { tarefas } = useSelector((state: RootReducer) => state);
+  const { tarefas } = useSelector((state: RootReducer) => state)
 
   return (
     <Container>
-      <p>
-        2 tarefas marcadas como: &quot;categoria&quot; e &quot;termo&quot;
-      </p>
+      <p>2 tarefas marcadas como: &quot;categoria&quot; e &quot;termo&quot;</p>
       <ul>
         {tarefas.map((t) => (
           <li key={t.titulo}>
             <Tarefa
               descricao={t.descricao}
-              titulo={t.titulo}
-              status={t.status}
-              prioridade={t.prioridade}
+              Titulo={t.titulo}
+              Status={t.status}
+              Prioridade={t.prioridade}
             />
           </li>
         ))}
       </ul>
     </Container>
-  );
-};
+  )
+}
 
-export default ListaDeTarefas;
+export default ListaDeTarefas
